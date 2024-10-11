e2100335deb53614dae02bed31b7e1e64ce8b1f1
vincentmungai <vinimunga@gmail.com>
2024-10-11 15:39:36 +0300

    redeploy

--- src/lib/fetchWorksItems.js
+++ src/lib/fetchWorksItems.js
@@ -3,13 +3,13 @@ import { items } from "@wix/data";
 const WixClient = createClient({
     modules: { items },
     auth: OAuthStrategy ({
-        clientId: import.meta.env.WIX_CLIENT_ID,
+        clientId: import.meta.env.WIX_CLIENT_ID
     }),
 });
 
 export default async function fetchWorksItems() {
     let query = WixClient.items.queryDataItems({
-        dataCollectionId: 'Import982',
+        dataCollectionId: 'Import982'
     });
     const works = await query.find();
     return works.items;
