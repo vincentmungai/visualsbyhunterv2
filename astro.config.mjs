d6a79972baeddc2ed034b0321fec39f2ca8c9e2c
vincentmungai <vinimunga@gmail.com>
2024-10-11 22:54:51 +0300

    redeploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
-export default defineConfig({
-    output: "server",
-    adapter: vercel(),
-    image: {
-      domains: ["https://supabase.com/"],
-    },
-    remotepatterns: [{ protocol: "https" }],
-    devToolbar: {
-        enabled: false
-    },
-    buildOptions: {
-        viteOptions: {
-            build: {
-                rollupOptions: {
-                    input: '/src/assets/portraits/*.{jpeg,jpg,png,gif,webp}',
-                },
-            },
-            assetsInclude: ['**/*.jpeg', '**/*.jpg', '**/*.png', '**/*.gif', '**/*.webp'],
-        },
-    },
-    redirects: {
-        '/works/isles-and-wedding-vows': '/projects/isles-and-wedding-vows',
-      },
-});
+export default defineConfig({});
