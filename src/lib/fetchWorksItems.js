7c78eff570805e58da675eec0e5e24a58e99084f
vincentmungai <vinimunga@gmail.com>
2024-10-11 15:43:38 +0300

    fetchWorksItems.js update

--- src/lib/fetchWorksItems.js
+++ src/lib/fetchWorksItems.js
@@ -1,16 +1,22 @@
-import {createClient, OAuthStrategy} from "@wix/sdk";
+import { createClient, OAuthStrategy } from "@wix/sdk";
 import { items } from "@wix/data";
+
 const WixClient = createClient({
     modules: { items },
-    auth: OAuthStrategy ({
-        clientId: import.meta.env.WIX_CLIENT_ID
+    auth: OAuthStrategy({
+        clientId: import.meta.env.WIX_CLIENT_ID,
     }),
 });
 
 export default async function fetchWorksItems() {
-    let query = WixClient.items.queryDataItems({
-        dataCollectionId: 'Import982'
-    });
-    const works = await query.find();
-    return works.items;
+    try {
+        let query = WixClient.items.queryDataItems({
+            dataCollectionId: 'Import982',
+        });
+        const works = await query.find();
+        return works.items;
+    } catch (error) {
+        console.error('Error fetching works items:', error);
+        throw error; // Rethrow the error if needed
+    }
 }
