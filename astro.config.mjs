7074415873c6130659c747874013004993352871
vincentmungai <vinimunga@gmail.com>
2024-10-12 12:23:33 +0300

    rollback updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
-    output: "server",
-    adapter: vercel(),
-    image: {
-      domains: ["https://supabase.com/"],
-    },
-    remotepatterns: [{ protocol: "https" }],
-    devToolbar: {
-        enabled: false
-    },
-    buildOptions: {
-        viteOptions: {
-            build: {
-                rollupOptions: {
-                    input: '/src/assets/portraits/*.{jpeg,jpg,png,gif,webp}',
-                },
-            },
-            assetsInclude: ['**/*.jpeg', '**/*.jpg', '**/*.png', '**/*.gif', '**/*.webp'],
-        },
-    },
-    redirects: {
-        '/works/isles-and-wedding-vows': '/projects/isles-and-wedding-vows',
-      },
+    output: 'hybrid',  // You're using hybrid mode, which allows a mix of static and server-rendered pages.
+    adapter: vercel(), // Use the Vercel adapter for serverless deployments.
 });
